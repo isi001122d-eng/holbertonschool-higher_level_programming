88f965a86c21ea79aac218d184953f20d58c9d69
isi001122d-eng <isi001122d@gmail.com>
2026-02-15 22:48:45 +0400

    Update 5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def no_c(my_string):
+def no_c(my_string=""):
     new_string = ""
-    for i in my string:
-        if i != "c" and i !="C":
-            new_string += i
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
     return new_string
