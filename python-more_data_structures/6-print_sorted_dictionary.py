4130aab49fb34d3c154c5655c78566b606210e57
isi001122d-eng <isi001122d@gmail.com>
2026-02-20 11:00:22 +0400

    Update 6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for i in sorted(a_dictionary):
-        print("{}: {}".format(k, a_dictionary[k]))
+        print("{}: {}".format(sorted(a_dictionary))
