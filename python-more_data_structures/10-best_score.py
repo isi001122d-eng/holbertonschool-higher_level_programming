1b6d9b7cd585e960680e02e934d3a17f94bfea90
isi001122d-eng <isi001122d@gmail.com>
2026-02-20 11:47:45 +0400

    Update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,13 +3,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-
     best_key = None
-    best_r = float('-inf')  # Ən kiçik mümkün ədəd (mənfi sonsuzluq)
-
+    best_r = float('-inf')
     for key, value in a_dictionary.items():
         if value > best_r:
             best_r = value
             best_key = key
-            
     return best_key
