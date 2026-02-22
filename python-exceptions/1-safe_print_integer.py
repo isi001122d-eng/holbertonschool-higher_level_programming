ac3c4f9128a4943b6257eaa657c25df7c8c7ab6f
isi001122d-eng <isi001122d@gmail.com>
2026-02-22 20:16:48 +0400

    Update 1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,16 +1,6 @@
-def list_division(my_l_1, my_l_2, list_length):
-    new_list = []
-    for i in range(list_length):
-        try:
-            res = 0
-            try:
-                res = my_l_1[i] / my_l_2[i]
-            except ZeroDivisionError:
-                print("division by 0")
-            except IndexError:
-                print("out of range")
-            except (TypeError, ValueError):
-                res = 0
-        finally:
-            new_list.append(res)
-    return new_list
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
