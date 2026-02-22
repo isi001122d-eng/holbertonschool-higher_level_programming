3e4f3bdf221bfd1f778715eb94b82dce32536eda
isi001122d-eng <isi001122d@gmail.com>
2026-02-22 19:48:34 +0400

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
-def safe_print_list_integers(my_list=[], x=0):
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]))
-                count += 1
-        except (ValueError, TypeError, IndexError):
-            continue
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print("")
     return count
