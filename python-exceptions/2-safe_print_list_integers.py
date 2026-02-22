252c809173c6be5f642fa0a5cdaff57a07a7341f
isi001122d-eng <isi001122d@gmail.com>
2026-02-22 20:46:04 +0400

    Update 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,6 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
+            if isinstance(my_list[i], bool):
+                raise ValueError
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
