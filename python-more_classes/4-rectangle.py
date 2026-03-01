dfacd4795d0cce445505328dffb99c8c1e5adf46
isi001122d-eng <isi001122d@gmail.com>
2026-03-01 20:57:40 +0400

    Update 4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -5,10 +5,14 @@
 class Rectangle:
     """Rectangle class definition"""
 
+    number_of_instances = 0
+    print_symbol = "#"
+
     def __init__(self, width=0, height=0):
         """Initialize rectangle instance"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -48,21 +52,39 @@ class Rectangle:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Compare two rectangles"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """Create square instance"""
+        return cls(size, size)
+
     def __str__(self):
         """Print rectangle instance"""
         if self.__width == 0 or self.__height == 0:
             return ""
         rect_str = ""
         for i in range(self.__height):
-            rect_str += ("#" * self.__width)
+            rect_str += (str(self.print_symbol) * self.__width)
             if i != self.__height - 1:
                 rect_str += "\n"
         return rect_str
 
     def __repr__(self):
         """Represent rectangle instance"""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        # Burada self.width və self.height (public) istifadə olunmalıdır
+        return "Rectangle({}, {})".format(self.width, self.height)
 
     def __del__(self):
         """Delete rectangle instance"""
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
