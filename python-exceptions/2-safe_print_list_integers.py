1e25453e70b4782ed24abe757cba2c4beb59a71e
isi001122d-eng <isi001122d@gmail.com>
2026-03-01 13:18:33 +0400

    Update 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+"""
+Bu modul siyahıdakı integerləri çap edən funksiyanı saxlayır.
+"""
+
 
 def safe_print_list_integers(my_list=[], x=0):
+    """
+    Siyahının ilk x elementindən yalnız integer olanları çap edir.
+    """
     count = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], bool):
-                raise ValueError
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print("")
     return count
