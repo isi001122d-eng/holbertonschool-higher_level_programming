9ea2d943488176bcb26dc0b75712854f6ab7b85d
isi001122d-eng <isi001122d@gmail.com>
2026-03-01 20:59:06 +0400

    Update 4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -5,14 +5,10 @@
 class Rectangle:
     """Rectangle class definition"""
 
-    number_of_instances = 0
-    print_symbol = "#"
-
     def __init__(self, width=0, height=0):
         """Initialize rectangle instance"""
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -50,23 +46,7 @@ class Rectangle:
         """Return rectangle perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width * 2) + (self.__height * 2)
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Compare two rectangles"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """Create square instance"""
-        return cls(size, size)
+        return (2 * self.__width) + (2 * self.__height)
 
     def __str__(self):
         """Print rectangle instance"""
@@ -74,17 +54,11 @@ class Rectangle:
             return ""
         rect_str = ""
         for i in range(self.__height):
-            rect_str += (str(self.print_symbol) * self.__width)
+            rect_str += ("#" * self.__width)
             if i != self.__height - 1:
                 rect_str += "\n"
         return rect_str
 
     def __repr__(self):
         """Represent rectangle instance"""
-        # Burada self.width və self.height (public) istifadə olunmalıdır
         return "Rectangle({}, {})".format(self.width, self.height)
-
-    def __del__(self):
-        """Delete rectangle instance"""
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...")
