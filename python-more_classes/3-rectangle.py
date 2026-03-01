3bca44b732b3e73eea48270826d8e55f13585f82
isi001122d-eng <isi001122d@gmail.com>
2026-03-01 21:09:10 +0400

    Update 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     """Rectangle class definition"""
 
     def __init__(self, width=0, height=0):
-        """Initialize rectangle instance"""
+        """Init rectangle instance"""
         self.width = width
         self.height = height
 
@@ -18,7 +18,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Setter for width"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Setter for height"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -52,13 +52,4 @@ class Rectangle:
         """Print rectangle instance"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        rect_str = ""
-        for i in range(self.__height):
-            rect_str += ("#" * self.__width)
-            if i != self.__height - 1:
-                rect_str += "\n"
-        return rect_str
-
-    def __repr__(self):
-        """Represent rectangle instance"""
-        return "Rectangle({}, {})".format(self.width, self.height)
+        return (("#" * self.__width + "\n") * self.__height)[:-1]
