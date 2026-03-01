fef4f5cbee8a377218f2bcc4f4619704fa48379f
isi001122d-eng <isi001122d@gmail.com>
2026-03-01 13:14:15 +0400

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Bu Modul docstring-idir (Faylın başında olur)"""
+
 class Square:
+    """Bu Class docstring-idir (Sinfin daxilində olur)"""
+
     def __init__(self, size):
+        """Bu Metod docstring-idir (Funksiyanın daxilində olur)"""
         self.__size = size
