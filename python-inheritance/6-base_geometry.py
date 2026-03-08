495c92542c301086b32fbb4ac77a1b8cabec46ac
isi001122d-eng <isi001122d@gmail.com>
2026-03-08 22:45:42 +0400

    Create 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry module task"""
+
+
+class BaseGeometry:
+    """Improved geometry class"""
+
+    def area(self):
+        """Method not implemented"""
+        raise Exception("area() is not implemented")
