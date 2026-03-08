930f138dedde66b38aff131f7e0de7188bd51aaf
isi001122d-eng <isi001122d@gmail.com>
2026-03-08 23:30:46 +0400

    Update task_00_abc.py

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-"""Lookup module"""
+"""Abstract Animal Class module"""
+from abc import ABC, abstractmethod
 
 
-def lookup(obj):
-    """Returns the list of available attributes and methods"""
-    return dir(obj)
+class Animal(ABC):
+    """Abstract Animal class definition"""
+
+    @abstractmethod
+    def speak(self):
+        """Abstract method that must be implemented by subclasses"""
+        pass
+
+class Dog(Animal):
+    """Dog subclass that inherits from Animal"""
+
+    def speak(self):
+        """Implementation of speak for Dog"""
+        return "Woof!"
+
+class Cat(Animal):
+    """Cat subclass that inherits from Animal"""
+
+    def speak(self):
+        """Implementation of speak for Cat"""
+        return "Meow!"
