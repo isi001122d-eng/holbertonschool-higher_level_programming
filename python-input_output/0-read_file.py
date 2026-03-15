04c86aa9d90a567f7410f199ce2d914ee7cc4951
isi001122d-eng <isi001122d@gmail.com>
2026-03-15 20:46:49 +0400

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Mətn faylını oxuyan modulu təmsil edir
+"""
+
+
 def read_file(filename=""):
+    """
+    UTF-8 formatında olan faylı oxuyur və stdout-a çap edir
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
