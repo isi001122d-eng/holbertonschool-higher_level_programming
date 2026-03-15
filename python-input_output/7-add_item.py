f43a5d29bd9889a5344d691839904af5dd6c7a7b
isi001122d-eng <isi001122d@gmail.com>
2026-03-15 20:54:45 +0400

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,13 +11,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-# Əgər fayl varsa, içindəkiləri yükləyirik, yoxdursa boş siyahı yaradırıq
 if os.path.exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
 
-# Komanda sətrindən gələn arqumentləri siyahıya əlavə edirik (skriptin adını - sys.argv[0] çıxmaqla)
 items.extend(sys.argv[1:])
 
 # Yenilənmiş siyahını fayla yazırıq
