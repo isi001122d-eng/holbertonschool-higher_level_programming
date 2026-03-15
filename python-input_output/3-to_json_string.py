e3f47412e7e6a01f500edcb28d0102a3a5e641a0
isi001122d-eng <isi001122d@gmail.com>
2026-03-15 20:50:36 +0400

    Create 3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Obyekti JSON string-ə çevirən modul
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Obyektin JSON təmsilini qaytarır
+    """
+    return json.dumps(my_obj)
