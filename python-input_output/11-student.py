b8cdaa6716d2f17a5d7d50759da5c33db64208c2
isi001122d-eng <isi001122d@gmail.com>
2026-03-15 20:58:10 +0400

    Create 11-student.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Yeniləmə funksiyası olan Student sinfi
+"""
+
+
+class Student:
+    """
+    Tələbə məlumatlarını saxlayan, filtrləyən və yeniləyən sinif
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Tələbə obyektini inisializasiya edir
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Atributları siyahıya əsasən filtrləyib lüğət kimi qaytarır
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            res = {}
+            for a in attrs:
+                if a in self.__dict__:
+                    res[a] = self.__dict__[a]
+            return res
+        return self.__dict__
+
+    def reload_from_json(self, json_data):
+        """
+        Lüğətdəki məlumatlarla obyektin atributlarını əvəz edir
+        """
+        for key, value in json_data.items():
+            setattr(self, key, value)
