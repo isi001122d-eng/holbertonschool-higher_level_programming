f9f941e1fde29ec6eca60739d2f72094c6b6ef8f
isi001122d-eng <isi001122d@gmail.com>
2026-03-15 20:53:49 +0400

    Create 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Arqumentləri oxuyub JSON faylına əlavə edən skript
+"""
+import sys
+import os.path
+
+# Əvvəlki tapşırıqlardan funksiyaları import edirik
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+# Əgər fayl varsa, içindəkiləri yükləyirik, yoxdursa boş siyahı yaradırıq
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+# Komanda sətrindən gələn arqumentləri siyahıya əlavə edirik (skriptin adını - sys.argv[0] çıxmaqla)
+items.extend(sys.argv[1:])
+
+# Yenilənmiş siyahını fayla yazırıq
+save_to_json_file(items, filename)
