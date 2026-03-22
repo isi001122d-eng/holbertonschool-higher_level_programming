ae90e68d20b4b7411319315db3f2dc45ce96e433
isi001122d-eng <isi001122d@gmail.com>
2026-03-22 23:55:40 +0400

    Update 14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""Script that prints all City objects from the database hbtn_0e_14_usa"""
+"""
+Prints all City objects from the database hbtn_0e_14_usa.
+"""
 import sys
-from model_state import Base, State
-from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    # Verilənlər bazasına qoşulma mühərriki yaradılır
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
+    # Sessiya yaradılır
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    rows = session.query(State, City).filter(
-        State.id == City.state_id).order_by(City.id).all()
+    # State və City cədvəllərini id-yə görə birləşdirib (Join) çəkirik
+    results = session.query(State, City).filter(State.id == City.state_id)\
+                     .order_by(City.id.asc()).all()
 
-    for state, city in rows:
+    # Nəticələri tələb olunan formatda çap edirik
+    for state, city in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
-```
-
-**Çıxış:**
-```
-California: (1) San Francisco
-California: (2) San Jose
-California: (3) Los Angeles
-...
