51a09f76c1b816647974259c4698111507cbf69f
isi001122d-eng <isi001122d@gmail.com>
2026-03-22 21:14:36 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
-"""
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Verilənlər bazasına qoşuluruq
-    # sys.argv[1]: username, sys.argv[2]: password, sys.argv[3]: database
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -17,17 +13,12 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
 
-    # Kursor yaradırıq ki, SQL əmrlərini icra edək
     cursor = db.cursor()
-    
-    # Şəkildəki tələbə uyğun SQL sorğusu
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Bütün sətirləri alırıq
     rows = cursor.fetchall()
+
     for row in rows:
         print(row)
 
-    # Kursoru və bazanı bağlayırıq
     cursor.close()
     db.close()
