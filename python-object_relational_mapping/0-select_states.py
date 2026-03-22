1ea1afdefd17a6f20370f1e5f7682cbd03aed026
isi001122d-eng <isi001122d@gmail.com>
2026-03-22 20:38:27 +0400

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0d_usa """
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Terminaldan gələn arqumentləri alırıq (username, password, database)
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    # Verilənlər bazasına qoşuluruq
+    # sys.argv[1]: username, sys.argv[2]: password, sys.argv[3]: database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Kursor yaradırıq ki, SQL əmrlərini icra edək
+    cursor = db.cursor()
     
-    cur = db.cursor()
-    # SQL sorğusunu icra edirik
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    
-    # Bütün nəticələri gətiririk
-    rows = cur.fetchall()
+    # Şəkildəki tələbə uyğun SQL sorğusu
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Bütün sətirləri alırıq
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
-    
-    # Bağlantıları bağlayırıq
-    cur.close()
+
+    # Kursoru və bazanı bağlayırıq
+    cursor.close()
     db.close()
