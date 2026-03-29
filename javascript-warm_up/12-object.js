6d258e109eb44a2752e687b0a664b0be72e72a35
isi <isi001122d@gmail.com>
2026-03-29 19:31:58 +0000

    12

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+/* SƏNİN KODUN BURADAN BAŞLAYIR */
+myObject.value = 89;
+/* SƏNİN KODUN BURADA BİTİR */
+
+console.log(myObject);
