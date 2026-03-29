dd4d98fb33c65bbe808d7cec71676f765d2c7e1f
isi001122d-eng <isi001122d@gmail.com>
2026-03-29 23:35:21 +0400

    Update 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,13 +4,9 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  
   const args = process.argv.slice(2).map(Number);
-  
   // Massivi böyükdən kiçiyə sıralayırıq
-  
   const uniqueSorted = [...new Set(args)].sort((a, b) => b - a);
-  
   // İkinci elementi çap edirik (əgər varsa)
   if (uniqueSorted.length < 2) {
     console.log(0);
